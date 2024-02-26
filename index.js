5ec93d31341b19434fe4778fd73ec695f33c565e
Khurshidbek Kobilov <kabilovhurshid5@gmail.com>
2024-02-27 03:07:04 +0500

    add support for spa parsing with IpakYuliBank example

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+const axios = require("axios").default;
+const cheerio = require("cheerio");
+const puppeteer = require("puppeteer");
 
 require("dotenv").config();
 
@@ -12,16 +15,49 @@ async function countToken(input) {
   return response.totalTokens;
 }
 
+async function scrapeSpaContent(url, element) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url, { waitUntil: "networkidle2" });
+
+  // Wait for a specific element to be present on the page
+  await page.waitForSelector(element);
+
+  // Get the HTML content after the SPA has fully loaded and executed JavaScript
+  const html = await page.content();
+
+  await browser.close();
+
+  return html;
+}
+
+async function parseHTML(url, element, spa = false) {
+  let response = null;
+
+  if (spa) {
+    response = await scrapeSpaContent(url, element);
+  } else {
+    response = await axios.get(url);
+  }
+
+  const $ = cheerio.load(response);
+
+  return $(element).html();
+}
+
 async function run() {
-  // For text-only input, use the gemini-pro model
-  const prompt = "Write a story about a magic backpack.";
-  const tokenCount = await countToken(prompt);
-  console.log("TOKEN COUNT " + tokenCount);
+  const html = await parseHTML(
+    "https://en.ipakyulibank.uz/physical/exchange-rates/courses",
+    "#__layout .application--wrap table",
+    true
+  );
+
+  const prompt = `Parse exchange rates from the html table below and return response as JSON: "${html}"`;
+  console.log(prompt);
 
   const result = await model.generateContent(prompt);
-  const response = result.response;
-  const text = response.text();
-  console.log(text);
+  const response = result.response.text();
+  console.log(response);
 }
 
 run();
